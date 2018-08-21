723a9876e48ec7f7c75c44d4b4287d632cb6c624
David Gonzalez Casin <david.gonzalezcasin@diva-e.com>
2018-08-21 22:07:44 +0200

    Moved generating the hexagons to another class and added some randomness

--- src/Map.js
+++ src/Map.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
-import { HexGrid, Layout, Hexagon, GridGenerator, Pattern } from 'react-hexgrid';
+import { HexGrid, Layout, Hexagon, Pattern } from 'react-hexgrid';
 import './Map.css';
 
 class Map extends Component {
-  render() {
-    const hexagons = GridGenerator.orientedRectangle(30, 20);
-    const hexagonSize = { x: 2, y: 2 };
 
-    return (
-      <div className="Map">
-        <HexGrid width={1200} height={1000} viewBox="-10 -10 100 100">
-          <Layout size={hexagonSize}>
-            { hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} fill="tree" />) }
-          </Layout>
-          <Pattern id="tree" link="https://classroomclipart.com/images/gallery/Clipart/Trees/TN_seasonal-tree-green-summer-clipart-720.jpg" size={hexagonSize} />
-        </HexGrid>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hexagons: this.props.hexagons,
+            hexagonSize: this.props.hexagonSize
+        }
+    }
+
+    createHexagon(hex, i) {
+        var fill = Math.random() >= 0.5;
+        if (fill) {
+            return <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} fill="tree" />;
+        }
+        return <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />;
+    }
+
+    render() {
+        return (
+            <div className="Map">
+                <HexGrid width={1200} height={1000} viewBox="-10 -10 100 100">
+                    <Layout size={this.state.hexagonSize}>
+                        {this.state.hexagons.map((hex, i) => this.createHexagon(hex, i))}
+                    </Layout>
+                    <Pattern id="tree" link="https://classroomclipart.com/images/gallery/Clipart/Trees/TN_seasonal-tree-green-summer-clipart-720.jpg" size={this.state.hexagonSize} />
+                </HexGrid>
+            </div>
+        );
+    }
 }
 
 export default Map;
--- src/MapGenerator.js
+++ src/MapGenerator.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { GridGenerator } from 'react-hexgrid';
 import Map from './Map'
 
 class MapGenerator extends Component {
   render() {
+    
+    const hexagons = GridGenerator.orientedRectangle(30, 20);
+    const hexagonSize = { x: 2, y: 2 };
+
     return (
       <div className="MapGenerator">
         <h1>Map Generator for a hexgrid map</h1>
-        <Map/>  
+        <Map hexagons={hexagons} hexagonSize={hexagonSize} />  
       </div>
     );
   }
